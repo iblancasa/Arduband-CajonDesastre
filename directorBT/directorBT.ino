3606b55cabd0df58fdf0357366437aec01865140
Israel Blancas <iblancasa@gmail.com>
2015-06-15 14:24:03 +0200

    Arreglando problemas al emitir

--- directorBT/directorBT.ino
+++ directorBT/directorBT.ino
@@ -1,24 +1,31 @@
 #include <SoftwareSerial.h> 
 #include <XBee.h>
-
+#include "TimerOne.h"
 
 XBee xbee;
 
-SoftwareSerial BT(10,11);
+//SoftwareSerial BT(10,11);
  
 void setup()
 {
+  Timer1.initialize(500); 
+  Timer1.attachInterrupt(Envio);
   xbee = XBee();
-  BT.begin(9600); 
+
   Serial.begin(9600);
-  xbee.setSerial(Serial);
+  Serial1.begin(9600);
+  xbee.setSerial(Serial1);
+  
+  while (!Serial1) {  }
+  //BT.begin(9600); 
+  
 }
 
-uint8_t payload[] = { '~','A','~' };
+uint8_t payload[] = { 0x7F,'<',0x7F };
 
-XBeeAddress64 primera = XBeeAddress64(0x13A200, 0x4079E3ED);
-XBeeAddress64 segunda = XBeeAddress64(0x13A200, 0x40A09C69);
-XBeeAddress64 tercera = XBeeAddress64(0x13A200, 0x40A4D173);
+XBeeAddress64 primera = XBeeAddress64(0x0013A200, 0x4079E3ED);
+XBeeAddress64 segunda = XBeeAddress64(0x0013A200, 0x40A09C69);
+XBeeAddress64 tercera = XBeeAddress64(0x0013A200, 0x40A4D173);
 
 uint16_t addr16=0xffff;
 uint8_t broadcastradius=0;
@@ -30,41 +37,51 @@ ZBTxRequest segundaS=ZBTxRequest(segunda,addr16,broadcastradius,option,payload,s
 ZBTxRequest terceraS=ZBTxRequest(tercera,addr16,broadcastradius,option,payload,sizeof(payload),frameid);
 
 
-long tiempo;
+long tiempo=0;
 int i=0;
 bool a = false;
 char recibido;
 
+uint8_t tempo='>';
+unsigned long periodo=980;
  
 void Envio(void){
-    if(millis()-tiempo>500){
-       if(a)
-         xbee.send(primeraS);
-       else
+    if(millis()-tiempo>periodo){
+       // if(a)
+       xbee.send(primeraS);
+     /*  else
          xbee.send(segundaS);
-       a=!a;
+       a=!a;*/
        tiempo=millis();
+       Serial.println("ENVIADO");
      }
 }
  
 
-
-
-uint8_t tempo;
-unsigned long periodo;
-
 void loop()
-{
- 
-  
-  if(BT.available())
+{ 
+ // noInterrupts();
+/*  if(BT.available()>0)
   {
-    recibido = BT.read();
-    tempo = (uint8_t) recibido;
+
+
+ 
+
+   recibido = BT.read();
+   Serial.println(recibido);
+ /*  tempo = (uint8_t) recibido;
+    
+    
     float aux = (float)tempo/60;
     periodo=(long)1000/aux;
-    Serial.println(periodo);
-  }
- 
+    payload[1] = tempo;
+    
+     primeraS=ZBTxRequest(primera,addr16,broadcastradius,option,payload,sizeof(payload),frameid);
+     segundaS=ZBTxRequest(segunda,addr16,broadcastradius,option,payload,sizeof(payload),frameid);
+   terceraS=ZBTxRequest(tercera,addr16,broadcastradius,option,payload,sizeof(payload),frameid);
 
+    Serial.println(periodo);
+*/
+//  }
+  //interrupts();
 }
